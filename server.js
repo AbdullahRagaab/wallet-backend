a444832bb3201c7b18175ac4338a1d90d6f7d5ab
Abdullah Ragab <abdullahragab460@gmail.com>
2025-11-29 02:03:42 +0200

    Update db.js

--- server.js
+++ server.js
@@ -18,12 +18,16 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
-    credentials: true,
-  }),
-);
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL || 'http://localhost:5173',
+//     credentials: true,
+//   }),
+// );
+app.use(cors({ origin: '*', credentials: true }));
+app.get('/', (req, res) => res.send('Server is running!'));
+
+
 app.use(helmet());
 app.use(express.json({ limit: '1mb' }));
 app.use(morgan('dev'));
